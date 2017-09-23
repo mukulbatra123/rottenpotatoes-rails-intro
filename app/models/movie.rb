aa43688a62c9aae8bc596ec89a6a8a82b02d92a8
Mukul <mbatra1@binghamton.edu>
2017-09-23 20:00:10 +0000

    yes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ActiveRecord::Base
+
+  def self.re_order(para)
+    self.order(para)
+  end
+
+  def self.get_ratings
+    rating_arr = Array.new
+    movies = Movie.all
+    movies.each do |movie|
+       rating_arr.push movie[:rating]
+       rating_arr.uniq!
+    end
+    return rating_arr
+  end
 end
